45fee8639b9af7867fb5c92de231c5ccea1c354f
Vincent G-C <vincent.jacques@hotmail.fr>
2020-06-04 15:09:31 +0200

    removed target

--- target/classes/sqlMisc/evenement.sql
+++
@@ -1,1 +0,0 @@
-create table evenement (id integer primary key, name varchar, contenuArticle varchar, image varchar, lienVideo varchar, date_evenement varchar, heure varchar, lieu varchar, organisateur varchar);
